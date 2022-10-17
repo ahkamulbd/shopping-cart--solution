e14dbbed2ad4c089e604c659a821c7b156e70c3a
ahkamulbd <ahkamulbd@gmail.com>
2022-10-17 16:02:42 +0600

    Function used to solve to get total case number

--- js/count.js
+++ js/count.js
@@ -1,5 +1,6 @@
 //console.log('This is count.js File');
 
+/*
 document.getElementById('btn-case-plus').addEventListener('click', function () {
     //console.log('Plus Button Clicked');
 
@@ -10,7 +11,6 @@ document.getElementById('btn-case-plus').addEventListener('click', function () {
     const newCaseNumber = previousCaseNumber + 1;
 
     caseNumberField.value = newCaseNumber;
-
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
@@ -23,5 +23,40 @@ document.getElementById('btn-case-minus').addEventListener('click', function () 
     const newCaseMinusNumber = previousCaseNumber - 1;
 
     caseNumberField.value = newCaseMinusNumber;
+})
+*/
 
+// Solution using function:
+
+function updateCaseNumber(isIncrease) {
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+
+    if (isIncrease) {
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else {
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true);
+
+    const caseTotalPrice = newCaseNumber * 59;
+
+    const caseTotalElement = document.getElementById('case-total');
+
+    caseTotalElement.innerText = caseTotalPrice;
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    updateCaseNumber(false);
 })
