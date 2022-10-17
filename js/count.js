3b5d96daada6c1369f0aae9a65436bd3160a21d3
ahkamulbd <ahkamulbd@gmail.com>
2022-10-17 15:23:03 +0600

    shopping-cart project initiated

---
+++ js/count.js
@@ -0,0 +1,27 @@
+//console.log('This is count.js File');
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    //console.log('Plus Button Clicked');
+
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    const newCaseNumber = previousCaseNumber + 1;
+
+    caseNumberField.value = newCaseNumber;
+
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+    //console.log('Minus Button Clicked');
+
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    const newCaseMinusNumber = previousCaseNumber - 1;
+
+    caseNumberField.value = newCaseMinusNumber;
+
+})
